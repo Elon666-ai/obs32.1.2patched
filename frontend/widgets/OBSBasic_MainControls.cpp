6d014652b4130f556b18805dd59f1dfb6a52ce34
Greg <td@lottoplay.ph>
2026-08-06 10:09:17 +0800

    fix: point Help > Release Notes at this fork's GitHub releases page
    Was still linking to github.com/obsproject/obs-studio/releases.

--- frontend/widgets/OBSBasic_MainControls.cpp
+++ frontend/widgets/OBSBasic_MainControls.cpp
@@ -437,7 +437,7 @@ void OBSBasic::on_actionShowWhatsNew_triggered()
 
 void OBSBasic::on_actionReleaseNotes_triggered()
 {
-	QString addr("https://github.com/obsproject/obs-studio/releases");
+	QString addr("https://github.com/Elon666-ai/obs32.1.2patched/releases");
 	QUrl url(QString("%1/%2").arg(addr, obs_get_version_string()), QUrl::TolerantMode);
 	QDesktopServices::openUrl(url);
 }
